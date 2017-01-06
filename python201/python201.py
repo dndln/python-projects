49260f1c0dfec25a0f1be05ab548cb071115bd06
Andy Kwong <andy.kwong@kinesis.org>
2017-01-06 16:29:48 +1100

    Completed Part I of python201

--- python201/python201.py
+++ python201/python201.py
@@ -682,8 +682,85 @@
 #         end=match.end())
 #     print(s)
 
-# type hinting - declaring function arguments and returned objects
-# to have a certain type.
-# This is not binding
-def some_function(number:int, name:str) -> None:
-    print('%s entered %s' % (name, number))
+# # type hinting - declaring function arguments and returned objects
+# # to have a certain type.
+# # This is not binding
+# def some_function(number:int, name:str) -> None: # expects an int and a string, and is expected to return None
+#     print('%s entered %s' % (name, number))
+
+# # no type hinting
+# def process_data(my_list, name):
+#     if name in my_list:
+#         return True
+#     else:
+#         return False
+#
+# if __name__ == '__main__':
+#     my_list = ['Mike', 'Nick', 'Toby']
+#     print(process_data(my_list, 'Mike'))
+#     print(process_data(my_list, 'John'))
+
+# # type hinting
+# def process_data(my_list: list, name:str) -> bool:
+#     return name in my_list
+#
+# if __name__ == '__main__':
+#     my_list = ['Mike', 'Nick', 'Toby']
+#     print(process_data(my_list, 'Mike'))
+#     print(process_data(my_list, 'John'))
+
+# # type hints can be built-in classes, abstract base classes, user defined classes, and types in the 'types' module
+# # create our own class and add a hint
+# class Fruit:
+#     def __init__(self, name, color):
+#         self.name = name
+#         self.color = color
+#
+# def salad(fruit_one: Fruit, fruit_two: Fruit) -> list:
+#     print(fruit_one.name)
+#     print(fruit_two.name)
+#     return [fruit_one, fruit_two]
+#
+# if __name__ == '__main__':
+#     f = Fruit('orange', 'orange')
+#     f2 = Fruit('apple', 'red')
+#     salad(f, f2)
+
+# you can also create aliases of classes:
+# Animal = str # Animal now stands for str
+#
+# def zoo(animal: Animal, number: int) -> None:
+#     print('The zoo has {} {}'.format(number, animal))
+#
+# if __name__ == '__main__':
+#     zoo('Zebras', 10)
+
+# # type hinting in overloaded functions
+# from functools import singledispatch
+#
+# @singledispatch
+# def add(a, b):
+#     raise NotImplementedError('Unsuppoted type')
+#
+# @add.register(int)
+# def _(a:int, b:int) -> int:
+#     print('First arg is of type ', type(a))
+#     print(a + b)
+#     return a + b
+#
+# @add.register(str)
+# def _(a:str, b:str) -> str:
+#     print('First arg is of type ', type(a))
+#     print(a + b)
+#     return a + b
+#
+# @add.register(list)
+# def _(a:list, b:list) -> list:
+#     print('First arg is of type ', type(a))
+#     print(a + b)
+#     return a + b
+#
+# if __name__ == '__main__':
+#     add(1, 2)
+#     add('1', '2')
+#     add([1], [2])
